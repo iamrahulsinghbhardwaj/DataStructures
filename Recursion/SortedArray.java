76439f55a4f282e2c6a7a114fbae43209f203e97
Rahul <rahulsingh.bhardwaj@ecolab.com>
2024-03-25 12:29:10 +0530

    SortedArray

---
+++ Recursion/SortedArray.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+public class SortedArray {
+    public static void main(String args[]){
+        int arr1[]=new int[]{1,2,56,-1,6};
+        int arr2[]=new int[]{1,2,3,4,5};
+
+        boolean result=findSortedRecursion(arr1,1);
+        System.out.println(result);
+    }
+
+    public static boolean findSortedRecursion(int arr[],int index){
+        if(index==arr.length){
+            return true;
+        }
+
+        if(arr[index]<arr[index-1]){
+            return false;
+        }
+
+        return findSortedRecursion(arr, index+1);
+    }
+}
